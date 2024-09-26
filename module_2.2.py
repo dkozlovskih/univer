9f12e7ef0b5b60907f4d4e0e18cdf5313fefa844
dkozlovskih <paleko80@gmail.com>
2024-09-26 21:20:15 +0300

    условия

--- module_2.2.py
+++ module_2.2.py
@@ -1,10 +1,10 @@
-print('Сколько одинаковых чисел?')
+print('Все ли равны?')
 first = int(input('Введите число 1: '))
 second = int(input('Введите число 2: '))
 third = int(input('Введите число 3: '))
 if first == second and first == third and second == third:
     print(3)
-elif first == second or first == third or second == third:
+elif first == second and first == third and second != third:
     print(2)
 else:
     print(0)
