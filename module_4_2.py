9e0ffcda4a14b8f64b160ee9c1b6b298e8cf76ac
dkozlovskih <paleko80@gmail.com>
2024-10-18 21:12:26 +0300

    Add files via upload

--- module_4_2.py
+++ module_4_2.py
@@ -1,7 +1,3 @@
-#Создайте новую функцию test_function
-#Создайте внутри test_function другую функцию - inner_function, Эта функция должна печатать значение "Я в области видимости функции test_function"
-#Вызовите функцию inner_function внутри функции test_function
-#Попробуйте вызывать inner_function вне функции test_function и посмотрите на результат выполнения программы
 
 def test_function():
     def inner_function():
@@ -10,3 +6,5 @@ def test_function():
     inner_function()
 
 test_function()
+
+# inner_function() - вызывает ошибку.
